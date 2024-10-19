e1c77cda24c1d956a30d3eaa21d16f05a0afddeb
Felix Paulusma <felix.paulusma@gmail.com>
2024-10-19 10:25:01 +0200

    small documentation and CI adjustment

--- src/Data/SafeJSON.hs
+++ src/Data/SafeJSON.hs
@@ -7,7 +7,7 @@ Stability   : experimental
 
 Please read the
 
-__[README on GitHub](https://github.com/Vlix/safe-json/blob/v1.1.0/README.md)__
+__[README on GitHub](https://github.com/Vlix/safe-json/blob/master/README.md)__
 
 for an extensive explanation of this library, why and how to use it,
 and examples.
